d144d5134b4e400591e3d849b91a6cac77292f9a
samitup <sami.tuppurainen@metropolia.fi>
2020-01-29 15:15:43 +0200

    backend

---
+++ eclipse-workspace/LaskutusohjelmaBackEnd/Laskutusohjelma/src/main/java/Backend/Laskutusohjelma/App.java
@@ -0,0 +1,13 @@
+package Backend.Laskutusohjelma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
